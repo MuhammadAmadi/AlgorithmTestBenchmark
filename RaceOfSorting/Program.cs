c1501afa17f2a2ad0ae6184621796deed76a66f4
Muhammad <type.92@mail.ru>
2023-03-30 22:30:09 +0300

    Revert "бенчмарка 1.3.1"
    This reverts commit f4877e65768efda840aff0585c184e4f9e47b5e8.

--- RaceOfSorting/Program.cs
+++ RaceOfSorting/Program.cs
@@ -10,9 +10,20 @@ namespace RaceOfSorting
 {
     internal class Program
     {
+        private class CustomConfig : ManualConfig
+        {
+            public CustomConfig()
+            {
+                AddJob(new Job());
+                AddValidator(JitOptimizationsValidator.DontFailOnError);
+                AddLogger(ConsoleLogger.Default);
+                AddColumnProvider(DefaultColumnProviders.Instance);
+            }
+        }
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<ListOfSortingBenchmark>();
+            BenchmarkRunner.Run<ListOfSortingBenchmark>(new CustomConfig());
+            Console.ReadLine();
         }
     }
 }
